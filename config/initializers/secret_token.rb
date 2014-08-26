3231ca07d8984138f989736fc3bb11dcdeaf922e
growthcode <growthcode@gmail.com>
2014-08-26 16:36:09 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'd8e8e63e2aca9fd130e7e8dd2794008901442acec967f89d527324fba6cc39292a8864f92d84b7c303af8f5bf4a55b999ff0a086006dae4384b51c036f8855b3'
